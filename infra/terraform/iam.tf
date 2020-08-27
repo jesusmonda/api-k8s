099b964a474e3f9d0df7987d718819ddb0b46c87
Jesús Monda <jesusmonda9@gmail.com>
2020-08-27 22:35:17 +0200

    remove external dns

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -70,7 +70,6 @@ resource "aws_iam_role_policy" "main" {
 POLICY
 }
 
-
 //EKS NODE
 resource "aws_iam_role" "eks_node" {
   name = "${var.project_name}_eks-node"
@@ -286,51 +285,4 @@ resource "aws_iam_role" "eks_cluster" {
 resource "aws_iam_role_policy_attachment" "eks_cluster" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster.name
-}
-
-//EXTERNAL DNS
-resource "aws_iam_role" "external_dns" {
-  name = "${var.project_name}_external-dns"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
-resource "aws_iam_role_policy" "external-dns" {
-  role = aws_iam_role.external_dns.name
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "route53:ChangeResourceRecordSets"
-      ],
-      "Resource": [
-        "arn:aws:route53:::hostedzone/*"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "route53:ListHostedZones",
-        "route53:ListResourceRecordSets",
-        "route53:*"
-      ],
-      "Resource": [
-        "*"
-      ]
-    }
-  ]
-}
-POLICY
 }
